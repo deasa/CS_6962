1cbb365196415bfc276f00b5bb6cd672b800432b
Brendan Ashton <bgeorgeashton@gmail.com>
2018-10-24 08:58:13 -0600

    finished first draft of parsedNum

--- Homework/Homework 4/parse_int.cpp
+++ Homework/Homework 4/parse_int.cpp
@@ -1,16 +1,41 @@
 #include<iostream>
 #include<string>
 #include<limits>
+#include<ctype.h>
 
 void ParseInt()
 {
+    std::string inputString = GetArgument();
+    bool sign;
+    bool hasSign = false;
+    double parsedNum;
+    if (inputString[0] == '-'|| inputString[0] == '+') hasSign = true;
+
+    for(size_t i = 0; i < inputString.size(); i++)
+    {
+        if (hasSign && i == 0) {
+            sign = inputString[0] == '+';
+            continue;
+        }
+        else if (isDigit(inputString[i])) {
+            parsedNum = parsedNum * 10 + (inputString[i] - '0');
+        }
+        else
+        {
+            std::cout << "Parsing failed";
+            return;
+        }
+    }
+    if (!sign) {
+        parsedNum = 0 - parsedNum;
+    }
+    std::cout << parsedNum;
 }
 
 int GetNumCases()
 {
 	int response = 0;
 	std::cin >> response;
-	// std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	return response;
 }
 
@@ -18,7 +43,6 @@ std::string GetArgument()
 {
 	std::string arg;
     std::cin >> arg;
-	// std::getline(std::cin, arg);
     return arg;
 }
 
