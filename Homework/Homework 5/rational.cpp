57a32dea628775d8efa7c17d0849c57d8147603c
Brendan Ashton <bgeorgeashton@gmail.com>
2018-11-02 23:05:08 -0600

    finished first draft

--- Homework/Homework 5/rational.cpp
+++ Homework/Homework 5/rational.cpp
@@ -9,71 +9,120 @@ private:
 public:
 	Rational()
 	{
-		//default constructor
+		num = 0;
+		denom = 0;
 	}
 
 	Rational(int n, int d)
 	{
+		num = n;
+		denom = d;
+		normalize();
 		//You will also need to normalize() the number to make sure that GCD(num, denom) == 1 and denom > 0.
 	}
 
 	Rational(int n)
 	{
+		num = n;
+		denom = 1;
 		//This constructor will need to turn an int into a rational number.
 	}
 
 	~Rational()
 	{
-		//delete num and denom
 	}
 
-	Rational Rational::operator+(Rational lhs, Rational rhs)
+	Rational operator+(Rational lhs, Rational rhs)
 	{
-
+		int lNum, lDenom, rNum, rDenom;
+		lNum = lhs.get_numerator();
+		lDenom = lhs.get_denominator();
+		rNum = rhs.get_numerator();
+		rDenom = rhs.get_denominator();
+		
+		if (lDenom != rDenom) {
+			lNum = lNum * rDenom;
+			rNum = rNum * lDenom;
+			lDenom = lDenom * rDenom;
+			rDenom = rDenom * lDenom;
+		}
+		Rational result = Rational((lNum + rNum), lDenom)
+		return result;
 	}
 
 	Rational operator-(Rational lhs, Rational rhs)
 	{
-
+		int lNum, lDenom, rNum, rDenom;
+		lNum = lhs.get_numerator();
+		lDenom = lhs.get_denominator();
+		rNum = rhs.get_numerator();
+		rDenom = rhs.get_denominator();
+		
+		if (lDenom != rDenom) {
+			lNum = lNum * rDenom;
+			rNum = rNum * lDenom;
+			lDenom = lDenom * rDenom;
+			rDenom = rDenom * lDenom;
+		}
+		Rational result = Rational((lNum - rNum), lDenom)
+		return result;
 	}
 
 	Rational operator*(Rational lhs, Rational rhs)
 	{
-
+		int lNum, lDenom, rNum, rDenom;
+		lNum = lhs.get_numerator();
+		lDenom = lhs.get_denominator();
+		rNum = rhs.get_numerator();
+		rDenom = rhs.get_denominator();
+		return Rational((lNum * rNum), (lDenom * rDenom));
 	}
 
 	Rational operator/(Rational lhs, Rational rhs)
 	{
-
+		int lNum, lDenom, rNum, rDenom;
+		lNum = lhs.get_numerator();
+		lDenom = lhs.get_denominator();
+		rNum = rhs.get_numerator();
+		rDenom = rhs.get_denominator();
+		return Rational((lNum * rDenom), (lDenom * rNum));
 	}
 
 	bool operator==(Rational lhs, Rational rhs)
 	{
-
-	}
-
-	bool operator<(Rational lhs, Rational rhs)
-	{
-		return toDecimal(lhs) < toDecimal(rhs);
+		return lhs.toDecimal() == rhs.toDecimal();
 	}
 
 	std::ostream& operator<<(std::ostream& output, Rational a)
 	{
-
+		output << num << '/' << denom;
+		return output;
 	}
 
-	int Rational::get_numerator()
+	int gcd(int n, int d) 
+	{ 
+		if (n == 0) 
+			return d; 
+		return gcd(d % n, n); 
+	} 
+
+	int get_numerator()
 	{
 		return num;
 	}
 
-	int Rational::get_denominator()
+	int get_denominator()
 	{
 		return denom;
 	}
 
 	void normalize()
 	{
+		while(gcd(num, denom) > 1){
+			int divisor = gcd(num, denom);
+			num = num / divisor;
+			denom = denom / divisor;
+		}
 		// reduce num and denom until the greatest common denominator is 1
 	}
 
@@ -81,17 +130,12 @@ public:
 	{
 		return num / denom;
 	}
+
+	bool operator<(Rational lhs, Rational rhs)
+	{
+		return toDecimal(lhs) < toDecimal(rhs);
+	}
 };
-
-
-
-
-
-double Rational::toDecimal()
-{
-	return num / denom;
-	//convert the current rational number to decimal format
-}
 
 int GetNumCases()
 {
@@ -107,6 +151,19 @@ int GetArgument()
     return arg;
 }
 
+void RationalOperations()
+{
+	Rational lhs = Rational(GetArgument(), GetArgument());
+	Rational rhs = Rational(GetArgument(), GetArgument());
+	std::cout << lhs + rhs + " ";
+	std::cout << lhs - rhs + " ";
+	std::cout << lhs * rhs + " ";
+	std::cout << lhs / rhs + " ";
+	std::cout << lhs < rhs + "\n";
+	std::cout << lhs.toDecimal() + " ";
+	std::cout << rhs.toDecimal() + "\n";
+}
+
 void PrintCaseStatement(int i)
 {
 	std::cout << "Case " << i << ":\n";
@@ -118,6 +175,6 @@ int main()
 	for(int i = 0; i < numCases; i++)
 	{
 		PrintCaseStatement(i);
-       	encrypt();
+       	RationalOperations();
 	}
 }
