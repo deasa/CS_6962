d497bfc941239e5833da10fe959d5763fcecfa02
deasa <bgeorgeashton@gmail.com>
2018-11-04 22:46:53 -0700

    Finished matrix.cpp

--- Homework/Homework 5/matrix.cpp
+++ Homework/Homework 5/matrix.cpp
@@ -4,30 +4,36 @@
 #include<vector>
 #include<cmath>
 
+int GetIntArgument()
+{
+	int arg;
+	std::cin >> arg;
+	return arg;
+}
+
 class Matrix
 {
-	private:
-        unsigned int numRows;
-        unsigned int numCols;
+	public:
+		int numRows;
+        int numCols;
         std::vector<std::vector<double>> _matrix;
 
-	public:
 		Matrix(int r, int c)
 		{
-            numRows = r;
-            numCols = c;
-            _matrix.resize(numRows);
+            this->numRows = r;
+            this->numCols = c;
+            _matrix.resize(this->numRows);
             for (int i = 0; i < _matrix.size(); i++)
             {
-                _matrix[i].resize(numCols, 0);
+                _matrix[i].resize(this->numCols, 0);
             }
             //create r x c matrix
 		}
 
 		Matrix()
 		{
-            numRows = 0;
-            numCols = 0;
+            this->numRows = 0;
+            this->numCols = 0;
             _matrix.resize(0);
             _matrix[0].resize(0, 0);
             //create 0x0 matrix
@@ -35,112 +41,108 @@ class Matrix
 
 		~Matrix()
         {
-            delete[] _matrix;
+			//for (int i = 0; i < this->numRows; i++)
+			//{
+			//	delete[] & this->_matrix[i];
+			//}
             //deallocate the memory which holds matrix elements
             //delete[] data;
         }
 
-        int GetNumRows()
+        inline Matrix& operator=(Matrix &m)
         {
-            return numRows;
-        }
-
-        int GetNumCols()
-        {
-            return numCols;
-        }
-
-        Matrix& operator=(const Matrix &m)
-        {
-            delete[] _matrix;
-            numRows = m.GetNumRows();
-            numCols = m.GetNumCols();
-            Matrix(numRows, numCols);
+            delete[] &this->_matrix;
+            this->numRows = m.numRows;
+            this->numCols = m.numCols;
+            Matrix(this->numRows, this->numCols);
             
-            for(int i = 0; i < numRows * numCols; i++)
-            {
-                this->[i] = m[i];
-            }
-            
-            //delete data
-            //set new size equal to m's sizes
-            //initialize new matrix
-            //loop and copy data elements
+			this->_matrix = m._matrix;
+
             return *this;
         }
 
-        Matrix operator*(const Matrix &n)
+        inline Matrix operator*(Matrix &n)
         {
-            Matrix prodMatrix(numRows, numCols);
-            std::vector<double> mRow, nCol;
-            
-            for(int i = 0; i < this->numCols; i++)
-            {
-                for(int j = 0; j < numCols; j++)
-                {
-                    mRow[j] = _matrix[j][i];
-                }
-                nCol = n._matrix[i];
-                for(int k = 0; k < numCols; k++)
-                {
-                    prodMatrix._matrix[i][k] = mRow[k] * nCol[k];
-                }
-            }
+            Matrix prodMatrix(this->numRows, n.numCols);
+
+			for (int k = 0; k < this->numRows; k++)
+			{
+				int l = 0;
+				for (int i = 0; i < n.numCols; i++)
+				{
+					double res = 0;
+					for (int j = 0; j < this->numCols; j++)
+					{
+						res += this->_matrix[k][j] * n._matrix[j][l];
+					}
+					prodMatrix._matrix[k][i] = res;
+					l += 1;
+				}
+			}
             
             return prodMatrix;
             //multiplies two matrices and returns new matrix
         }
 
-        Matrix operator+(const Matrix &m)
+        inline Matrix operator+(Matrix &m)
         {
-            Matrix resultMatrix(numRows, numCols);
+            Matrix resultMatrix(this->numRows, this->numCols);
             
-            for(int i = 0; i < numRows; i++)
-            {
-                
-                for(int j = 0; j < numCols; j++)
-                {
-                    resultMatrix._matrix(i, j) = this->_matrix[i][j] + m._matrix(i,j);
-                }
-            }
+			for (int i = 0; i < resultMatrix.numRows; i++)
+			{
+				for (int j = 0; j < resultMatrix.numCols; j++)
+				{
+					resultMatrix._matrix[i][j] = this->_matrix[i][j] + m._matrix[i][j];
+				}
+			}
             return resultMatrix;
             //adds two matrices and returns new matrix
         }
 
-        double operator[](int &i)
+        inline double operator[](int &i)
         {
-            int cRow, cCol, index;
-            if (i > numCols){
-                cRow = std::floor(i/numCols);
+            int cRow, cCol;
+            if (i > this->numCols - 1){
+                cRow = static_cast<int>(std::floor(i/this->numCols));
             }
             else
             {
-                cRow = 1;
+                cRow = 0;
             }
-            cCol = i % numCols;
-            return _matrix(cRow, cCol);
+			cCol = i - (cRow * this->numCols);
+			return operator()(cRow, cCol);
             //return the ith element of the matrix, reading L-R then Top-Bottom
         }
 
-        double operator()(int &i, int &j)
+        inline double operator()(int i, int j)
         {
-            return _matrix[i][j];
+            return this->_matrix[i][j];
             //return the i,jth element of the matrix. i is the row, j is the column
         }
 
-        std::ostream& operator<<(std::ostream& stream, const Matrix& matrix) {
-            for(int i = 0; i < numCols; i++)
-            {
-                for(int j = 0; j < numRows; j++)
-                {
-                    stream << _matrix(i, j);
-                }
-            }
+        friend std::ostream& operator<<(std::ostream& stream, Matrix& matrix) {
+			for (int i = 0; i < matrix.numCols * matrix.numRows; i++)
+			{
+				if (((i + 1) % matrix.numCols == 0) && i != matrix.numCols * matrix.numRows) {
+					stream << matrix[i] << "\n";
+				}
+				else {
+					stream << matrix[i] << " ";
+				}
+			}
             return stream;
         }
 
-        void operator>>()
-        {
+        friend std::istream& operator>>(std::istream& stream, Matrix& matrix)
+        {	
+			for (int i = 0; i < matrix.numRows; i++)
+			{
+				for (int j = 0; j < matrix.numCols; j++)
+				{
+					matrix._matrix[i][j] = GetIntArgument();
+				}
+			}
+			return stream;
             //reads a matrix
         }
 };
@@ -159,45 +161,27 @@ std::string GetStringArgument()
     return arg;
 }
 
-int GetIntArgument()
-{
-    int arg;
-    std::cin >> arg;
-    return arg;
-}
-
 void MatrixOperation()
 {
+	int m1Rows, m1Cols, m2Rows, m2Cols;
     std::string operation = GetStringArgument();
     m1Rows = GetIntArgument();
     m1Cols = GetIntArgument();
     Matrix m1 = Matrix(m1Rows, m1Cols);
-    
-    for(int i = 0; i < m1Cols; i++)
-    {
-        for(int j = 0; j < m1Rows; j++)
-        {
-            m1(i, j) = GetIntArgument();
-        }   
-    }
+	std::cin >> m1;
     
     m2Rows = GetIntArgument();
     m2Cols = GetIntArgument();
     Matrix m2 = Matrix(m2Rows, m2Cols);
+	std::cin >> m2;
 
-    for(int i = 0; i < m2Cols; i++)
-    {
-        for(int j = 0; j < m2Rows; j++)
-        {
-            m2(i, j) = GetIntArgument();
-        }   
-    }
-    
     if (operation == "multiply") {
-        std::cout << m1 * m2;
+		Matrix m3 = m1 * m2;
+        std::cout << m3;
     }
-    else if (operation = "add") {
-        std::cout << m1 + m2;
+    else if (operation == "add") {
+		Matrix m4 = m1 + m2;
+        std::cout << m4;
     }
 }
 
