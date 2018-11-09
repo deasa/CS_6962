8438b865c18a88830732cdd7f8717c995b28a539
Brendan Ashton <bgeorgeashton@gmail.com>
2018-11-08 21:21:08 -0700

    moved firstDay, finished first draft of find_min_max

---
+++ Homework 6/find_min_max.py
@@ -0,0 +1,21 @@
+numCases = int(input())
+
+def PrintCaseStatement(i):
+    print('Case: ' + str(i))
+
+def FindMinMax():
+    results = input().split()
+    nums = [int(i) for i in results]
+    minimum = nums[0]
+    maximum = nums[0]
+    for n in nums:
+        if n > maximum:
+            maximum = n
+        elif n < minimum:
+            minimum = n
+    print('Min: ' + str(minimum))
+    print('Max: ' + str(maximum))
+
+for i in range(0, numCases):
+    PrintCaseStatement(i)
+    FindMinMax()
