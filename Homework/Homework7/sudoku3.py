dab105a2dddf0f6919f0e4bd89919cf5b41fa810
deasa <bgeorgeashton@gmail.com>
2018-11-30 22:58:58 -0700

    removed debug aides

--- Homework/Homework7/sudoku3.py
+++ Homework/Homework7/sudoku3.py
@@ -1,16 +1,12 @@
 import sudokuHelper as helper
 
 path = input("Enter input filepath: ")
-command = 'y'
+matrix = helper.ReadInFile(path)
+helper.Print(matrix)
 
-while command == 'y':
-    matrix = helper.ReadInFile(path)
-    helper.Print(matrix)
+row = int(input("Enter row: "))
+col = int(input("Enter column: "))
+num = int(input("Enter number: "))
 
-    row = int(input("Enter row: "))
-    col = int(input("Enter column: "))
-    num = int(input("Enter number: "))
-
-    helper.UpdateGridWithNum(matrix, row, col, num)
-    helper.Print(matrix)
-    command = input("Continue? y/n: ")
+helper.UpdateGridWithNum(matrix, row, col, num)
+helper.Print(matrix)
